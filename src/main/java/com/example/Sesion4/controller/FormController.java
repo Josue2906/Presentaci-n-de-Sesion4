67b6c0cff9a04fb8ff94dbfe3f15cf51ec335ab7
Josue Gonzales <u00030785@sise.com.pe>
2024-11-27 22:21:28 -0500

    Presentación Sesion4

---
+++ src/main/java/com/example/Sesion4/controller/FormController.java
@@ -0,0 +1,27 @@
+package com.example.Sesion4.controller;
+
+import com.example.Sesion4.model.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+
+@Controller
+public class FormController {
+    
+    @GetMapping("/form")    
+    public String showForm(Model model){
+        model.addAttribute("user", new User());
+        return "form";
+    }
+    
+    @PostMapping("/submit")
+    public String submitForm(@ModelAttribute User user, Model model){
+        model.addAttribute("user", user);
+        return "success";
+    }
+}
+
+
